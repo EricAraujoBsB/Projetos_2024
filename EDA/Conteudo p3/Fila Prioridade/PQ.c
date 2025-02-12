b5e2c20acfaf5b29b1f7cfb99c1079bc9b7fe74b
EricAraujoBsB <egericaraujo@gmail.com>
2025-02-12 18:07:29 -0300

    heap sort

--- EDA/Conteudo p3/Fila Prioridade/PQ.c
+++ EDA/Conteudo p3/Fila Prioridade/PQ.c
@@ -72,5 +72,91 @@ void fixDown(int k, int N){
     }
 }
 
+void PQinsert(Item v){
+    pq[++N] = v;
+    fixUp(N);
+}
+
+Item PQdelmax(){
+    //troca topo pelo ultimo
+    exch(pq[1], pq[N]);
+
+    //reposicione o ultimo
+    fixDown(1, N-1);
+
+    //devolva o ultimo
+    return pq[N--];
+}
+
+void PQsort(Item *v, int l, int r){
+    PQinit(r-l+1);
+
+    for(int k=l; k<=r; k++){
+        PQinsert(v[k]);
+    }
+    for(int k=r; k>=l; k--){
+        v[k] = PQdelmax();
+    }
+}
+
+void heap_sort(Item *v, int l, int r){
+    pq = v+l-1;
+    N = r-l+1;
+    for(int k=N/2; k>=1; k--){
+        fixDown(k, N);
+    }
+    while(N>1){
+        exch(pq[1], pq[N]);
+        fixDown(1, --N);
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    int n;
+    scanf("%d", &n);
+    if(argc>1){
+
+        //ordenacao
+        int *v = malloc(n*sizeof(int));
+        for(int i=0; i<n; i++) {
+            scanf("%d", &v[i]);
+        }
+
+        if(n<50) 
+            show(v, 0, n-1);
+
+        if(!strcmp(argv[1], "pqsort"))
+            PQsort(v, 0, n-1);
+        else if(!strcmp(argv[1], "heap"))
+            heap_sort(v, 0, n-1);
+
+        if(n<50) 
+            show(v, 0, n-1);
+
+        if(is_sorted(v, 0, n-1)) printf("ordenado.\n");
+        else printf("nao ordenado.\n");
+
+    } else {
+        //fila de prioridades
+        PQinit(n);
+        char e;
+        int b;
+        while(scanf(" %c", &e)){
+            if(e=='i'){
+                scanf("%d", &b);
+                if(N<n) PQinsert(b);
+                show(pq, 1, N);
+            } else if(e=='d') {
+                if((N+1)>0) PQdelmax();
+                show(pq, 1, N+1);
+            }
+        }
+    }
+    
+    
+    return 0;
+}
+
 
 
